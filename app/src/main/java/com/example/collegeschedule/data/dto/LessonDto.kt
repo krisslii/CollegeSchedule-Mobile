16876f29631b3eb743a8e2998ba6891ac6540d68
Роман <xxxxx.yyyyy.bbbbb@gmail.com>
2026-03-20 18:49:52 +0700

    Добавил компоненты пользовательского интерфейса: составление списка расписаний для отображения карточек расписаний

--- app/src/main/java/com/example/collegeschedule/data/dto/LessonDto.kt
+++ app/src/main/java/com/example/collegeschedule/data/dto/LessonDto.kt
@@ -1,4 +1,13 @@
 package com.example.collegeschedule.data.dto
 
-class LessonDto {
-}
+data class LessonDto(
+    val lessonNumber: Int,
+    val time: String,
+    val subject: String,
+    val teacher: String,
+    val teacherPosition: String,
+    val classroom: String,
+    val building: String,
+    val address: String,
+    val groupParts: Map<LessonGroupPart, LessonPartDto?>
+)
--- app/src/main/java/com/example/collegeschedule/utils/DateUtils.kt
+++ app/src/main/java/com/example/collegeschedule/utils/DateUtils.kt
@@ -8,7 +8,6 @@ fun getWeekDateRange(): Pair<String, String> {
     val today = LocalDate.now()
     val formatter = DateTimeFormatter.ISO_DATE
 
-    // Если сегодня воскресенье — стартуем с понедельника
     var start = if (today.dayOfWeek == DayOfWeek.SUNDAY) {
         today.plusDays(1)
     } else {
@@ -18,7 +17,6 @@ fun getWeekDateRange(): Pair<String, String> {
     var daysAdded = 0
     var end = start
 
-    // Нужно получить ровно 6 учебных дней (ПН–СБ)
     while (daysAdded < 5) {
         end = end.plusDays(1)
         if (end.dayOfWeek != DayOfWeek.SUNDAY) {

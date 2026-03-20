de321ef063724e3b2170ea368350492bc485ab2b
Роман <xxxxx.yyyyy.bbbbb@gmail.com>
2026-03-20 18:19:43 +0700

    Добавил репозиторий: ScheduleRepository для управления данными

---
+++ app/src/main/java/com/example/collegeschedule/data/repository/ScheduleRepository.kt
@@ -0,0 +1,18 @@
+package com.example.collegeschedule.data.repository
+
+import com.example.collegeschedule.data.api.ScheduleApi
+import com.example.collegeschedule.data.dto.ScheduleByDateDto
+
+class ScheduleRepository(private val api: ScheduleApi) {
+    suspend fun loadSchedule(
+        group: String,
+        start: String,
+        end: String
+    ): List<ScheduleByDateDto> {
+        return api.getSchedule(
+            groupName = group,
+            start = start,
+            end = end
+        )
+    }
+}
